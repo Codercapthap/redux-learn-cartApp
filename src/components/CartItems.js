558d59606428c07c72110c24dc8d4dcdfb5056ca
Codercapthap <kidkaito1412.1@gmail.com>
2022-07-31 08:28:19 +0700

    first commit

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,14 +1,29 @@
 import React from "react";
 import CartItem from "./CartItem";
 import "./Cart.css";
+import { useSelector } from "react-redux";
 const CartItems = () => {
+  const cartItems = useSelector((state) => {
+    return state.cart.itemsList;
+  });
   return (
     <div className="cart-container">
       <h2>Your Cart</h2>
       <ul>
-        <li>
-          <CartItem id={1} price={2500} name={"Macbook"} />
-        </li>
+        {cartItems.map((item) => {
+          return (
+            <li key={item.id}>
+              {" "}
+              <CartItem
+                quantity={item.quantity}
+                name={item.name}
+                total={item.totalPrice}
+                price={item.price}
+                id={item.id}
+              />{" "}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,27 @@
 import React from "react";
 import Header from "./Header";
 import Products from "./Products";
+import CartItems from "./CartItems";
 import "./Layout.css";
+import { useSelector } from "react-redux";
 const Layout = () => {
-  let total = 100;
+  let total = 0;
+  const itemsList = useSelector((state) => state.cart.itemsList);
+
+  total = itemsList.reduce((total, currentItem) => {
+    return total + currentItem.totalPrice;
+  }, 0);
+
+  console.log(total);
+
+  const showCart = useSelector((state) => state.cart.showCart);
 
   return (
     <React.Fragment>
       <div className="layout">
         <Header />
         <Products />
+        {showCart && <CartItems />}
         <div className="total-price">
           <h3>Total: ${total}</h3>
           <button className="orderBtn">Place Order</button>
